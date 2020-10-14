15eb07acf9080f7f3420a4da41233f4f6cbbe411
Denis <den_from_bykhov1988@mail.ru>
2020-10-15 02:14:48 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -39,6 +39,36 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let arrTmp = [];
+    for (let i = 0; i < expr.length; i += 10) {
+        arrTmp.push(expr.slice(i, i + 10))
+    }
+    arrTmp = arrTmp.map((item) => {
+        if (item !== '**********') {
+            return item.slice(item.indexOf("1"));
+        } else {
+            return " ";
+        }
+    })
+
+    arrTmp = arrTmp.map((item) => {
+        return item.replace(/(10)|(11)/g, function (match, p1, p2) {
+            if (p1) {
+                return ".";
+            }
+            if (p2) {
+                return "-";
+            }
+        });
+    })
+
+    return arrTmp.map((item) => {
+        if(item !== " ") {
+            return MORSE_TABLE[item];
+        } else {
+            return item;
+        }
+    }).join("");
 }
 
 module.exports = {
